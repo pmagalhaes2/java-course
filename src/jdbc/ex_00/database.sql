1daee7c21151dde43efcfac1380f05a592a06e09
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-09-23 18:27:10 -0300

    chore: inserindo dados do banco (INSERT)

--- src/jdbc/ex_00/database.sql
+++ src/jdbc/ex_00/database.sql
@@ -1,16 +1,16 @@
 CREATE TABLE department (
-  Id int(11) NOT NULL AUTO_INCREMENT,
+  Id SERIAL NOT NULL,
   Name varchar(60) DEFAULT NULL,
   PRIMARY KEY (Id)
 );
 
 CREATE TABLE seller (
-  Id int(11) NOT NULL AUTO_INCREMENT,
+  Id SERIAL NOT NULL,
   Name varchar(60) NOT NULL,
   Email varchar(100) NOT NULL,
-  BirthDate datetime NOT NULL,
-  BaseSalary double NOT NULL,
-  DepartmentId int(11) NOT NULL,
+  BirthDate DATE NOT NULL,
+  BaseSalary DECIMAL NOT NULL,
+  DepartmentId INTEGER NOT NULL,
   PRIMARY KEY (Id),
   FOREIGN KEY (DepartmentId) REFERENCES department (id)
 );
